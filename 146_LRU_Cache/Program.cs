0f9ca36edf75b3354a59a02e4805c19f7152dab3
l907793011 <35017088+l907793011@users.noreply.github.com>
2020-05-25 10:30:39 +0800

    56

---
+++ 146_LRU_Cache/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _146_LRU_Cache
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ LeetCodeCSharp/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "cbbd";
+            string s2 = LongestPalindrome(s);
+            Console.WriteLine(s2);
+            Console.ReadKey();
+        }
+
+        public static string LongestPalindrome(string s)
+        {
+            string sTest = "";
+            int n = s.Length;
+            bool[,] p = new bool[n,n];
+            for (int i = 0; i < n;++i)
+            {
+                for (int j = i; j>= 0;--j)
+                {
+                    if (s[i] != s[j])
+                    {
+                        p[i, j] = false;
+                    }
+                    else
+                    {
+                        int nLen = i-j;
+                        if (nLen <= 1 ||(nLen > 1 && p[i - 1, j + 1] ))
+                        {
+                            p[i, j] = true;
+                            if ( (nLen +1 ) > sTest.Length)
+                            {
+                                Console.WriteLine("j: " + j + " nLen: " + (nLen + 1));
+                                sTest = s.Substring(j, nLen + 1);
+                            }
+                        }
+                    }
+                }
+            }
+            return sTest;
+        }
+    }
+}

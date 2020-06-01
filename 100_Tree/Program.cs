d20765f1dac252219cb000a44607f38bd48ba88a
l907793011 <35017088+l907793011@users.noreply.github.com>
2020-06-01 16:36:23 +0800

    100.相同的树

---
+++ 100_Tree/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _100_Tree
+{
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int x) { val = x; }
+    }
+
+    public class Solution
+    {
+        public bool IsSameTree(TreeNode p, TreeNode q)
+        {
+            if (p == null && q == null)
+            {
+                return true;
+            }
+            else if (p == null || q == null)
+            {
+                return false;
+            }
+            if (p.val != q.val)
+            {
+                return false;
+            }
+            return IsSameTree(p.left, q.left) && IsSameTree(p.right, q.right);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ls1 = { 1, 0, 3 };
+            int[] ls2 = { 1, 0, 3 };
+            TreeNode p = new TreeNode(ls1[0])
+            {
+                left = new TreeNode(ls1[1]),
+                right = new TreeNode(ls1[2])
+            };
+
+            TreeNode q = new TreeNode(ls2[0]) {
+                left = new TreeNode(ls2[1]),
+                right = new TreeNode(ls2[2])
+            };
+            
+
+            Solution su = new Solution();
+            bool bVal = su.IsSameTree(p,q);
+            Console.WriteLine("XXX:  "+ bVal.ToString());
+            Console.ReadKey();
+        }
+    }
+}

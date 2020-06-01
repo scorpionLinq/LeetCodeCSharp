0344954ad1b85f7e279d13dcc58ab7db76a34130
l907793011 <35017088+l907793011@users.noreply.github.com>
2020-06-01 17:01:37 +0800

    104. 二叉树的最大深度

---
+++ 104_Tree_Deep/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _104_Tree_Deep
+{
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int x) { val = x; }
+    }
+
+    public class Solution
+    {
+        public int MaxDepth(TreeNode root)
+        {
+            if (root == null) return 0;
+            return 1 + Math.Max(MaxDepth(root.left), MaxDepth(root.right));
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ls = {3,9,20,15,7 };
+            TreeNode root = new TreeNode(ls[0]) {
+                left = new TreeNode(ls[1]),
+                right = new TreeNode(ls[2])
+                {
+                    left = new TreeNode(ls[3]),
+                    right = new TreeNode(ls[4])
+                }
+            };
+            Solution su = new Solution();
+            int nMax = su.MaxDepth(root);
+            Console.WriteLine("XX: "+ nMax.ToString());
+            Console.ReadKey();
+        }
+    }
+}
